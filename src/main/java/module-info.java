ec7653154d587780030b301d6fbdb450b2b00d2e
TeranS22 <teransenevirathne@gmail.com>
2024-11-15 21:51:15 +0530

    Initial commit - Set up

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.newsgenie2409084 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.newsgenie2409084 to javafx.fxml;
+    exports org.example.newsgenie2409084;
+}
